770af5065a1565c4f2e0c4c5837de20e97351a09
Martins Obayomi <obayomimartins96@gmail.com>
2021-08-18 11:48:51 +0100

    Update ngo & volunteer tables

--- src/database/models/volunteer.js
+++ src/database/models/volunteer.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Volunteer = sequelize.define('Volunteer', {
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
+    middlename: DataTypes.STRING,
     phonenumber: DataTypes.STRING,
     email: DataTypes.STRING,
     passwords: DataTypes.STRING,
@@ -14,7 +15,27 @@ module.exports = (sequelize, DataTypes) => {
     address: DataTypes.TEXT,
     sdgId: DataTypes.INTEGER,
     interestAreaId: DataTypes.INTEGER,
-    skillId: DataTypes.INTEGER
+    skillId: DataTypes.INTEGER,
+    profilePhoto: DataTypes.STRING,
+    headerPhoto: DataTypes.STRING,
+    bio: DataTypes.TEXT,
+    profession: DataTypes.STRING,
+    educationlevel: DataTypes.ENUM('primary', 'secondary', 'OND', 'HND', 'BSC', 'masters', 'PHD'),
+    availability: DataTypes.ENUM('full-time', 'part-time'),
+    hoursOrWeek: DataTypes.INTEGER,
+    daysofWeek: DataTypes.STRING,
+    prevOrganisationName: DataTypes.STRING,
+    prevProject: DataTypes.STRING,
+    prevOrganisationRole: DataTypes.STRING,
+    prevDesOfResponsibilities: DataTypes.TEXT,
+    currentPosition: DataTypes.STRING,
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE,
+    duration: DataTypes.INTEGER,
+    website: DataTypes.STRING,
+    linkedin: DataTypes.STRING,
+    instagram: DataTypes.STRING,
+    twitter: DataTypes.STRING,
   }, {});
   Volunteer.associate = function(models) {
     // associations can be defined here
