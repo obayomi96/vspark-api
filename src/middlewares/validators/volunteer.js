1a97bb988924243568693db96075ab2544a1dcb3
Martins Obayomi <obayomimartins96@gmail.com>
2021-09-09 13:58:52 +0100

    Volunteer validation

--- src/middlewares/validators/volunteer.js
+++ src/middlewares/validators/volunteer.js
@@ -1,4 +1,4 @@
-import { check } from 'express-validator';
+import { check, param, query } from 'express-validator';
 
 export const validateVolunteerAuth = [
   check('email', 'A valid email is required')
@@ -13,4 +13,71 @@ export const validateVolunteerAuth = [
     .isLength({ min: 4, max: 30 }),
 ];
 
-export default { validateVolunteerAuth };
+export const validateProfileFetch = [
+  param('volunteer_id').not().isEmpty().withMessage('Please parse a volunteer_id as param'),
+];
+
+export const validateProfileUpdate = [
+  param('volunteer_id').not().isEmpty().withMessage('Please parse a volunteer_id as param'),
+  check('email', 'A valid email is required')
+    .exists()
+    .isString()
+    .isEmail()
+    .isLength({ min: 5, max: 30 }),
+  check('firstname', 'firstname is required')
+    .exists()
+    .isString(),
+  check('lastname', 'lastname is required')
+    .exists()
+    .isString(),
+  check('country', 'country is required')
+    .exists()
+    .isString(),
+  check('state', 'state is required')
+    .exists()
+    .isString(),
+  check('city', 'city is required')
+    .exists()
+    .isString(),
+  check('phonenumber', 'Phonenumber is required')
+    .exists()
+    .isString(),
+  check('interestAreaId', 'interestAreaId is required')
+    .exists()
+    .isString(),
+  check('skillId', 'skillId is required')
+    .exists()
+    .isString(),
+  check('educationlevel', 'educationlevel is required')
+    .exists()
+    .isString(),
+  check('profession', 'profession is required')
+    .exists()
+    .isString(),
+  check('prevOrganisationName', 'prevOrganisationName is required')
+    .exists()
+    .isString(),
+  check('prevOrganisationRole', 'prevOrganisationRole is required')
+    .exists()
+    .isString(),
+  check('startDate', 'startDate is required')
+    .exists()
+    .isString(),
+  check('endDate', 'endDate is required')
+    .exists()
+    .isString(),
+  ];
+
+  export const validatePasswordReset = [
+    param('volunteer_id').not().isEmpty().withMessage('Please parse a volunteer_id as param'),
+  check('oldPassword', 'oldPassword is required')
+    .exists()
+    .isString()
+    .trim(),
+  check('newPassword', 'newPassword is required')
+    .exists()
+    .isString()
+    .trim(),
+  ];
+
+export default { validateVolunteerAuth, validateProfileFetch, validateProfileUpdate, validatePasswordReset };
